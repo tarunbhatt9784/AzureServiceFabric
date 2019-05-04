2030a23c4d0edb1ce6277edf09be67e6d75360c3
tarunbhatt9784 <32237362+tarunbhatt9784@users.noreply.github.com>
2019-05-04 22:27:32 +1000

    Draft

---
+++ Ecommerce/ECommerce.ProductCatalog.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ECommerce.ProductCatalog.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ Ecommerce/ECommerce.ProductCatalog.Common/CustomDataContractProvider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.ProductCatalog.Model;
+using Microsoft.ServiceFabric.Services.Remoting.V2;
+
+
+namespace ECommerceProductCatalog.Common
+{
+    public class CustomDataContractProvider : IServiceRemotingMessageSerializationProvider
+    {
+        private readonly ServiceRemotingDataContractSerializationProvider provider;
+        private readonly IEnumerable<Type> myTypes;
+
+        public CustomDataContractProvider()
+        {
+            this.provider = new ServiceRemotingDataContractSerializationProvider();
+            this.myTypes = new List<Type>()
+            {
+                typeof(Product)
+            };
+        }
+        public IServiceRemotingMessageBodyFactory CreateMessageBodyFactory()
+        {
+            return this.provider.CreateMessageBodyFactory();
+        }
+
+        public IServiceRemotingRequestMessageBodySerializer CreateRequestMessageSerializer(Type serviceInterfaceType,
+            IEnumerable<Type> requestBodyTypes)
+        {
+            var result = requestBodyTypes.Concat(this.myTypes);
+            return this.provider.CreateRequestMessageSerializer(serviceInterfaceType, result);
+        }
+
+        public IServiceRemotingRequestMessageBodySerializer CreateRequestMessageSerializer(Type serviceInterfaceType, IEnumerable<Type> requestWrappedTypes, IEnumerable<Type> requestBodyTypes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+
+        public IServiceRemotingResponseMessageBodySerializer CreateResponseMessageSerializer(Type serviceInterfaceType, IEnumerable<Type> responseWrappedTypes, IEnumerable<Type> responseBodyTypes = null)
+        {
+            var result = responseBodyTypes.Concat(this.myTypes);
+            return this.provider.CreateResponseMessageSerializer(serviceInterfaceType, result);
+        }
+    }
+}
